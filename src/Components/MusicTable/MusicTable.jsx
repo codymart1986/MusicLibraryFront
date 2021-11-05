07e1ffc96829b3550ff40cb7aff693c64a8dfcc7
codymart1986 <codymart1986@hotmail.com>
2021-11-05 09:07:36 -0700

    setting up music table

--- src/Components/MusicTable/MusicTable.jsx
+++ src/Components/MusicTable/MusicTable.jsx
@@ -1,3 +1,38 @@
-import React from 'react';
-import './MusicTable.css';
+import React from 'react'
+import './MusicTable.css'
 
+const MusicTable = (props) => {
+    return ( 
+        <div>
+            <h2>Song Library</h2>
+            <table className="styled-table">
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Album</th>
+                        <th>Artist</th>
+                        <th>Genre</th>
+                        <th>Release Date</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {props.songs.map((song) => {
+                        return(
+                            <tr className="active-row">
+                            <td>{song.title}</td>
+                            <td>{song.album}</td>
+                            <td>{song.artist}</td>
+                            <td>{song.genre}</td>
+                            <td>{song.releaseDate}</td>
+                            </tr>
+                        )}
+                        )}
+                </tbody>
+            </table>
+            
+        </div>
+    );
+}
+
+
+export default MusicTable
