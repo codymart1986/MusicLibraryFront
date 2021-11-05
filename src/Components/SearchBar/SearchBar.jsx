3d89bffb0aaced4bab33a97af1cfa8c150a50938
codymart1986 <codymart1986@hotmail.com>
2021-11-05 07:49:26 -0700

    working on searchBar

--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -1,2 +1,51 @@
 import React, { Component } from 'react';
 import './SearchBar.css';
+
+class SearchBar extends Component{
+    constructor(props){
+        super (props);
+        this.state = {
+            search: ""
+        }
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        let results = this.props.search.filter(song => {
+            if (song.title.toLowerCase()===this.state.searchTerm.toLowerCase()){
+                return song
+            }
+            if (song.album.toLowerCase()===this.state.searchTerm.toLowerCase()){
+                return song
+            }
+            if (song.artist.toLowerCase()===this.state.searchTerm.toLowerCase()){
+                return song
+            }
+            if (song.genre.toLowerCase()===this.state.searchTerm.toLowerCase()){
+                return song
+            }
+            if (song.releaseDate.toLowerCase()===this.state.searchTerm.toLowerCase()){
+                return song
+            }
+        });
+        this.setState({
+            search: ""
+        })
+        this.props.SearchBar(results)
+    }
+    render(){
+
+        return (
+
+            <div>
+                
+            </div>
+        )
+    }
+}
